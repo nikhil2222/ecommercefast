f6191283086274c672ef2277e7cc1d34905423ef
nikhilnk2206 <154123463+nikhilnk2206@users.noreply.github.com>
2026-02-08 17:22:17 +0530

    fixed

--- config/__init__.py
+++ config/__init__.py
@@ -11,7 +11,7 @@ DB_HOST = config.get("MONGO_DB_HOST", "mongo")
 DB_PORT = config.get("MONGO_DB_PORT", 27017)
 DB_NAME = config.get("MONGO_DB_NAME", "ecommerce")
 
-MONGO_URI = f"mongodb+srv://nikhilhumai:Nikhil220900@nkproject.uwv9d4w.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
+MONGO_URI = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@nkproject.uwv9d4w.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
 
 config["DB_NAME"] = DB_NAME
 config["MONGO_URI"] = MONGO_URI
